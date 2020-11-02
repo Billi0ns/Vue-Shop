316098e0e38611af486451d1068703267d304b0b
Billi0ns <hsuangus@DW-mbpr.local>
2020-11-02 16:43:42 +0800

    Changed default font-family and deleted no-index

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, PingFang TC, Microsoft JhengHei, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
